1808b31eb4f42024f0716a9aebb74ed1ce74ca66
Pharell77coder <titi.pharell77420@gmail.com>
2024-12-02 09:17:57 +0100

    Update home.php
    oublie du link global.css dans le home.php re-update

--- www/public/php/home.php
+++ www/public/php/home.php
@@ -42,6 +42,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Formulaire des utilisateurs </title>
         <link rel="stylesheet" href=".\..\css\header.css">
+        <link rel="stylesheet" href=".\..\css\global.css">
     </head>
     <body>
     <?php include './include/header.php'; ?>
